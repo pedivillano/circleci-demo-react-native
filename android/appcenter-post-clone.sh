3d2718c23dc659df9e1dc0e0e110a6c152ff506a
Pedi <75835901+pedivillano@users.noreply.github.com>
2024-07-10 10:01:36 +0800

    Update appcenter-post-clone.sh

--- android/appcenter-post-clone.sh
+++ android/appcenter-post-clone.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 echo "hello world!"
+pwd
+cd ..
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output index.android.bundle --sourcemap-output index.android.bundle.map
+cd-
+pwd
